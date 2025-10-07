8ef3e89c1daa7587fa80601122bca0949cc01166
Jason LIN <jasongm0520@gmail.com>
2025-10-07 23:10:01 +0900

    render pass

--- VulkanTest/VulkanTest.cpp
+++ VulkanTest/VulkanTest.cpp
@@ -268,7 +268,21 @@ private:
 
 		createImageViews();
 
+		createRenderPass();
+
 		createGraphicsPipeline();
+	}
+
+	void createRenderPass() {
+		VkAttachmentDescription colorAttachment{};
+		colorAttachment.format = swapChainImageFormat;
+		colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
+		colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR; // clear display always
+		colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE; // to display
+		colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+		colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+		colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED; // dont care previous layout
+		colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR; // operated when rendering end, swap chain start
 	}
 
 	void createGraphicsPipeline() {
